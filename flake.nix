a9d53d19cc4cb9444b37700cb65ae0ec9548e970
Henry Blanchette <henry@monkeyface.com>
2023-09-06 08:32:45 -0700

    macos support for nix develop

--- flake.nix
+++ flake.nix
@@ -14,5 +14,16 @@
          export NIX_GHC_LIBDIR=${myGhc}/lib/ghc-${myGhc.version}
        '';
      };
+    devShell.x86_64-darwin =
+      with nixpkgs.legacyPackages.x86_64-darwin;
+      let myGhc = ghc.withPackages(p : with p; [doctest deepseq ghc-heap-view QuickCheck]);
+      in mkShell rec {
+        name = "env";
+        packages = [ myGhc ghcid haskell-ci ];
+        shellHook = ''
+          export NIX_GHC=${myGhc}/bin/ghc
+          export NIX_GHC_LIBDIR=${myGhc}/lib/ghc-${myGhc.version}
+        '';
+     };
   };
 }
